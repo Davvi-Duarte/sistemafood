7a949ebec697f0b61ae4e16a543754e06b40d6f7
Davvi Duarte Rodrigues <davvi.duarte@gmail.com>
2024-08-16 15:32:34 -0300

    ajustes da navegação no sidebar
    criação de novos icons, definição dos  estilos de todo o projeto

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,6 +2,8 @@ import { Container } from "./styles"
 
 import { ReactComponent as BurguerIcon } from '../../assets/burger.svg'
 import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
+import { ReactComponent as SodaPopIcon } from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
 
 import menuImg from '../../assets/menu.svg'
 
@@ -13,9 +15,23 @@ export function Sidebar(){
     <nav>
       <ul>
         <li>
-          <a href="#">
-            <BurguerIcon />
-            <span>Hamburgueres</span>
+          <a>
+            <BurguerIcon /> <span>Hamburgueres</span>
+          </a>
+        </li>
+        <li>
+          <a>
+            <PizzaIcon /> <span>Pizzas</span>
+          </a>
+        </li>
+        <li>
+          <a>
+            <SodaPopIcon /> <span>Bebidas</span>
+          </a>
+        </li>
+        <li>
+          <a>
+            <IceCreamIcon /> <span>Sorvetes</span>
           </a>
         </li>
       </ul>
