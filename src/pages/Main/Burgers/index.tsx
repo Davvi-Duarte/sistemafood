34f72889ec172bb4f7f4cb6576bf2ebc83d9d50e
Davvi Duarte Rodrigues <davvi.duarte@gmail.com>
2024-08-16 22:05:24 -0300

    criando subpáginas dos produtos

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -1,3 +1,35 @@
+import { Head } from "../../../components/Head"
+
+import { SnackTitle } from "../../../components/SnackTitle"
+
 export default function Burgers(){
-  return <h1>Burgers</h1>
+  const data = [{
+
+    id:1,
+    snack: "burger",
+    name: 'Mega',
+    description:'Nosso Mega',
+    price: 25.5,
+    image: 'https://i.imgur.com/upjIUnG.jpg',
+  },
+
+  {
+
+    id:2,
+    snack: "burger",
+    name: 'Super',
+    description:'Nosso Super',
+    price: 25.5,
+    image: 'https://i.imgur.com/B4J04AJ.jpg',
+  },
+]
+
+console.log('data')
+
+  return(
+  <>
+    <Head title="Hambúrgueres" description="Nossos melhores Hamburgueres"/>
+    <SnackTitle>Hambúrgueres</SnackTitle>
+  </>
+  )
 }
