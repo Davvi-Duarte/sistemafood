57f41b2810d1272f6a3fb7dff073ec207358d166
Davvi Duarte Rodrigues <davvi.duarte@gmail.com>
2024-08-16 23:23:09 -0300

    configurando os templates de cada produto

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -1,35 +1,34 @@
 import { Head } from "../../../components/Head"
-
 import { SnackTitle } from "../../../components/SnackTitle"
+import { Snacks } from "../../../components/Snacks"
 
-export default function Burgers(){
-  const data = [{
+export default function Burgers() {
+  const data = [
+    {
+      id: 1,
+      snack: "burger",
+      name: "Mega",
+      description: "Nosso Mega",
+      price: 25.5,
+      image: "https://i.imgur.com/upjIUnG.jpg",
+    },
+    {
+      id: 2,
+      snack: "burger",
+      name: "Super",
+      description: "Nosso Super",
+      price: 25.5,
+      image: "https://i.imgur.com/B4J04AJ.jpg"
+    },
+  ]
 
-    id:1,
-    snack: "burger",
-    name: 'Mega',
-    description:'Nosso Mega',
-    price: 25.5,
-    image: 'https://i.imgur.com/upjIUnG.jpg',
-  },
+  console.log(data)
 
-  {
-
-    id:2,
-    snack: "burger",
-    name: 'Super',
-    description:'Nosso Super',
-    price: 25.5,
-    image: 'https://i.imgur.com/B4J04AJ.jpg',
-  },
-]
-
-console.log('data')
-
-  return(
-  <>
-    <Head title="Hambúrgueres" description="Nossos melhores Hamburgueres"/>
-    <SnackTitle>Hambúrgueres</SnackTitle>
-  </>
+  return (
+    <>
+      <Head title="Hambúrgueres" description="Nossos melhores Hambúrgueres" />
+      <SnackTitle>Hambúrgueres</SnackTitle>
+      <Snacks snacks={data} />
+    </>
   )
 }
